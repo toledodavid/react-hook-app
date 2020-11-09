8cc427d08e9481398c0333f0a0467e6b086c7a51
David Toledo <dtoledoz0207@gmail.com>
2020-11-09 12:51:21 -0600

    create-customHook-useCounter-and-use-it-in-CounterWithCustomHook-component

---
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
+import './counter.css';
+
+
+const CounterWithCustomHook = () => {
+
+  const {state, increment, decrement, reset} = useCounter(100);
+
+  return(
+    <>
+      <h1>Counter with Hook: {state}</h1>
+      <hr/>
+
+      <button onClick={() => increment(2)} className="btn btn-primary">+1</button>
+      <button onClick={() => decrement(2)} className="btn btn-danger">-1</button>
+      <button onClick={reset} className="btn btn-secondary">Reset</button>
+    </>
+  );
+}
+
+
+export default CounterWithCustomHook;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CounterApp from './components/01-useState/CounterApp';
 //import HookApp from './HookApp';
+//import CounterApp from './components/01-useState/CounterApp';
+import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 
 ReactDOM.render(
-  <CounterApp />,
+  <CounterWithCustomHook />,
   document.getElementById('root')
 );
