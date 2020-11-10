eb25c5eba5a8e501c299de319603213fbe64f426
David Toledo <dtoledoz0207@gmail.com>
2020-11-10 11:54:41 -0600

    create-warning-example-with-listeners-or-observables-with-useEffect-hook-in-Message-component

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const Message = () => {
+
+  const [coords, setCoords] = useState({x: 0, y: 0});
+
+  const {x, y} = coords;
+
 
   useEffect(() => {
     
     console.log('Message component mounted');
 
+    const mouseMove = (ev) => {
+      const coords = {x: ev.x, y: ev.y};
+      console.log(coords);
+      setCoords(coords);
+    }
+
+    window.addEventListener('mousemove', mouseMove);
+
     return () => {
       console.log('Message component DISMOUNTED');
+      window.removeEventListener('mousemove', mouseMove);
     }
   }, []); 
 
   return(
     <div>
       <h1>You're great</h1>
+
+      <p>X: {x}, Y: {y}</p>
     </div>
   );
 }
