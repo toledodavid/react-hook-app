bf588d0ac7a21df92eb73c97532112c93ae4d77d
David Toledo <dtoledoz0207@gmail.com>
2020-11-10 11:05:52 -0600

    create-SimpleForm-component-to-use-useEffect-hook-example

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import './effects.css';
+
+
+const SimpleForm = () => {
+
+  const [formState, setFormState] = useState({
+    name: '',
+    email: ''
+  });
+
+  const {name, email} = formState;
+
+  useEffect(() => {
+    console.log('Hey!');
+  }, []);
+
+  useEffect(() => {
+    console.log('email changed');
+  }, [email]);
+
+  const handleInputChange = ({target}) => {
+    setFormState({
+      ...formState,
+      [target.name]: target.value
+    });
+  }
+
+  return(
+    <>
+      <h1>useEffect</h1>
+      <hr/>
+
+      <div className="form-group">
+        <input type="text" name="name" className="form-control" placeholder="Your name" autoComplete="off" value={name} onChange={handleInputChange} />
+      </div>
+
+      <div className="form-group">
+        <input type="email" name="email" className="form-control" placeholder="email@example.com" autoComplete="off" value={email} onChange={handleInputChange} />
+      </div>
+    </>
+  );
+}
+
+export default SimpleForm;
