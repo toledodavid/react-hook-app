b9ff78dc236b524cbd4447f8147c37e45abb3ab7
David Toledo <dtoledoz0207@gmail.com>
2020-11-11 13:23:49 -0600

    create-RealExampleRef-component-to-use-real-example-for-useRef-hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
 export const useFetch = ( url ) => {
+
+  const isMounted = useRef(true);
+
   const [state, setState] = useState({data: null, loading: true, error: null});
+
+  useEffect(() => {
+    
+    return () => {
+      isMounted.current = false;
+    }
+  }, []);
   
   useEffect(() => {
 
     setState({data: null, loading: true, error: null});
 
     fetch(url).then(response => response.json()).then(data => {
-      setState({
-        loading: false,
-        error: null,
-        data
-      });
+      if (isMounted.current) {
+        setState({
+          loading: false,
+          error: null,
+          data
+        });
+      }
     });
   }, [url]);
 
