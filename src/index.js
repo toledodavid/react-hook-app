a396c5973e85bf95a28819da427e9b29a984bd3b
David Toledo <dtoledoz0207@gmail.com>
2020-11-13 12:16:49 -0600

    resolve-task-using-useCallback-hook-in-Padre-component-and-React.memo-in-Hijo-component

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ import ReactDOM from 'react-dom';
 //import Layout from './components/05-useLayoutEffect/Layout';
 //import Memorize from './components/06-memos/Memorize';
 //import MemoHook from './components/06-memos/MemoHook';
-import CallBackHook from './components/06-memos/CallBackHook';
+//import CallBackHook from './components/06-memos/CallBackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 
 ReactDOM.render(
-  <CallBackHook />,
+  <Padre />,
   document.getElementById('root')
 );
