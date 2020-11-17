c86d59ab76158d8b2eec38ecda9d3da47e8c8b2c
David Toledo <dtoledoz0207@gmail.com>
2020-11-17 17:08:16 -0600

    create-first-design-for-TodoApp-component

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -20,14 +20,43 @@ const TodoApp = () => {
 
   return(
     <div>
-      <h1>Todo App</h1>
+      <h1>Todo App ({todos.length})</h1>
       <hr/>
 
-      <ul>
-        <li>Hola</li>
-        <li>mundo</li>
-        <li>asd</li>
-      </ul>
+      <div className="row">
+
+        <div className="col-7">
+
+          <ul className="list-group list-group-flush">
+            {
+              todos.map((todo, index) => (
+                <li key={todo.id}
+                    className="list-group-item">
+                  <p className="text-center">{index + 1}. {todo.desc}</p>
+                  <button className="btn btn-danger">Delete</button>
+                </li>
+              ))
+            }
+          </ul>
+
+        </div>
+
+        <div className="col-5">
+          <h4>Add TODO</h4>
+          <hr/>
+
+          <form>
+            <input type="text" 
+                    name="description"
+                    placeholder="Learn..."
+                    autoComplete="off"
+                    className="form-control"/>
+
+            <button className="btn btn-outline-primary btn-block mt-1">Add</button>
+          </form>
+        </div>
+
+      </div>
     </div>
   );
 }
