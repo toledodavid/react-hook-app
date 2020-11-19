ed6ce6fd7946a77b5e00c52e314bcca57e16994c
David Toledo <dtoledoz0207@gmail.com>
2020-11-19 13:05:45 -0600

    using-Link-and-NavLink-in-NavBar-component

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
+
+import NavBar from './NavBar';
 
 import AboutScreen from './AboutScreen';
 import HomeScreen from './HomeScreen';
@@ -16,10 +19,14 @@ const AppRouter = () => {
     <Router>
       <div>
 
+        <NavBar />
+
         <Switch>
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/login" component={LoginScreen} />
           <Route exact path="/about" component={AboutScreen} />
+          
+          <Redirect to="/" />
         </Switch>
       </div>
     </Router>
