84e8c9d2bc9167b8fe1572e4edf7168061667353
David Toledo <dtoledoz0207@gmail.com>
2020-11-23 11:52:16 -0600

    create-test-for-increment-decrement-and-reset-functions-from-useCounter-customHook

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useCounter } from '../../hooks/useCounter';
 
 
@@ -7,11 +7,52 @@ describe('Tests in useCounter custom hook', () => {
 
   test('It should return default values', () => {
     const { result } = renderHook(() => useCounter());
-    console.log(result.current);
     expect(result.current.counter).toBe(10);
     expect(typeof result.current.increment).toBe('function');
     expect(typeof result.current.decrement).toBe('function');
     expect(typeof result.current.reset).toBe('function');
+  });
+
+  test('It should have counter value in 100', () => {
+    const { result } = renderHook(() => useCounter(100));
+    expect(result.current.counter).toBe(100);
+  });
+
+  test('It should increment counter by 1', () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+    });
+
+    const { counter } = result.current;
+    expect(counter).toBe(101);
+  });
+
+  test('It should decrement counter by 1', () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+
+    act(() => {
+      decrement();
+    });
+
+    const { counter } = result.current;
+    expect(counter).toBe(99);
+  });
+
+  test('It should reset counter value to 100', () => {
+    const { result } = renderHook(() => useCounter(100));
+    const {increment, reset} = result.current;
+
+    act(() => {
+      increment();
+      reset();
+    });
+
+    const {counter} = result.current;
+    expect(counter).toBe(100);
   });
   
 
